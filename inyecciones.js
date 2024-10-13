63b7b065045ee7d5c218337341bba00cf8d9f36a
Walter <106196739+waltercamino@users.noreply.github.com>
2024-10-13 19:57:48 -0300

    Se hizo cambios para el footer y la barra de navegacion sea responsive. Se agrego menu hamburguesa para ispositivos moviles

--- inyecciones.js
+++ inyecciones.js
@@ -11,7 +11,31 @@ window.onload = function() {
             // Insertar el contenido del header en el contenedor
             document.getElementById('header-container').innerHTML = data;
 
-            // Cargar el contenido de footer.html dentro del bloque then del header
+            // Agregar la funcionalidad del menú hamburguesa
+            const hamburger = document.querySelector('.hamburger');
+            const navLinks = document.querySelector('.nav-links');
+            const links = document.querySelectorAll('.nav-links li');
+
+            if (hamburger && navLinks) {
+                hamburger.addEventListener('click', () => {
+                    // Alternar la clase nav-active en el menú
+                    navLinks.classList.toggle('nav-active');
+
+                    // Animar los enlaces
+                    links.forEach((link, index) => {
+                        if (link.style.animation) {
+                            link.style.animation = '';
+                        } else {
+                            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                        }
+                    });
+
+                    // Animar el hamburger
+                    hamburger.classList.toggle('toggle');
+                });
+            }
+
+            // Cargar el contenido de footer.html
             return fetch('plantillas/footer.html');
         })
         .then(response => {
